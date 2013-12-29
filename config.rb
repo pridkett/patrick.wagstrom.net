3f7e81772853a6877ca3049ed2c8962dd110e69f
Patrick Wagstrom <patrick@wagstrom.net>
2013-12-28 21:27:34 -0500

    Refinements to index page
    Added in a sidebar. Abuse some of the features of kramdown to render
    markdown inside of block elements. Shrank the footer some.
    
    Signed-off-by: Patrick Wagstrom <patrick@wagstrom.net>

--- config.rb
+++ config.rb
@@ -33,10 +33,14 @@
 ###
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
 # activate :livereload
+
+# use the imageoptim middleman plugin to optimize all images
+# see: https://github.com/plasticine/middleman-imageoptim
+activate :imageoptim
 
 # Methods defined in the helpers block are available in templates
 helpers do
@@ -54,6 +58,51 @@ helpers do
     if arg == active
         " class=\"" + cssclass + "\""
     end
+  end
+  
+  SQUARE_HEIGHT = 170
+
+  def affiliation_square(img, url=nil)
+    if !img.has_key?(:width) && !img.has_key?(:height) && !img[:url].include?("://")
+        real_path = img[:url]
+        real_path = File.join(images_dir, real_path) unless real_path.start_with?('/')
+        full_path = File.join(source_dir, real_path)
+
+      if File.exists?(full_path)
+        begin
+          width, height = ::FastImage.size(full_path, :raise_on_failure => true)
+          img[:width]  = width
+          img[:height] = height
+        rescue FastImage::UnknownImageType
+          warn("Image not supported")
+          # No message, it's just not supported
+        rescue
+          warn "Couldn't determine dimensions for image #{path}: #{$!.message}"
+        end
+      end
+    end
+
+    margin_top = ((SQUARE_HEIGHT - img[:height])/2).to_i
+    margin_bottom = (SQUARE_HEIGHT - margin_top - img[:height])
+
+    img_style = "margin-top: #{margin_top}px; margin-bottom: #{margin_bottom}px;"
+
+    img_tag = image_tag(
+                        img[:url], 
+                        :itemprop => "image", 
+                        :alt => img[:alt], 
+                        :title => img[:title],
+                        :width => img[:width],
+                        :height => img[:height],
+                        :style => img_style
+    )
+  
+    warn("Image: #{img}")
+    # if we have a URL then link the image with link_to
+    unless url.nil?
+      img_tag = link_to img_tag, url
+    end
+    '<li class="previous-affiliation"><div class="affiliation-square">' + img_tag + '</div></li>'
   end
 end
 

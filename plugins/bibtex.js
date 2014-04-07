734f13124b4001f53aed7d880c26850e751f6eb8
Patrick Wagstrom <patrick@wagstrom.net>
2014-04-06 21:55:41 -0400

    Add support for Zotero BibTeX files
    Zotero has a quirk where BibTeX files are written improperly and have
    values like 'month = mar' where it should be 'month = {mar}'. Well, at
    least that's what the javascript bibtex parser I'm using is
    expecting. This transforms the input to ensure that it passes this
    check.
    
    Also added a little bit of documentation.
    
    Signed-off-by: Patrick Wagstrom <patrick@wagstrom.net>

--- plugins/bibtex.js
+++ plugins/bibtex.js
@@ -4,12 +4,37 @@ var bibtex = require("bibtex-parse-js");
 module.exports = function(env, callback) {
     env.helpers.bibtex = {};
 
+    /**
+     * opens up a bibtex file and returns the content
+     *
+     * this is intentionally a synchronous method as the calling
+     * functions inside of jinja templates don't appear to handle
+     * asynchronous methods.
+     *
+     * @param {string{ filename the filename on the local filesystem to parse
+     *
+     * @return an {Array} of objects representing the bibtex output
+     */
     env.helpers.bibtex.parse = function(filename) {
 	var text = fs.readFileSync(filename, "utf8");
+	// work around a bug in Zotero where not all months are enclosed in braces
+	text = text.replace(/month\s*=\s*([a-zA-Z]+)/g,"month = {$1}");
 	var json = bibtex.toJSON(text);
 	return json;
     };
 
+    /**
+     * filters an array of BibTeX entries based on either the presence
+     * of a keyword or an arbitrary function
+     *
+     * @param {Array} data an array of BibTeX entries, usually obtained from parse
+     *
+     * @param filter if a string then include only those entries with
+     * that keyword set. If a function then use that function as the
+     * filter function for the data.
+     *
+     * @return the filtered {Array} of entries
+     */
     env.helpers.bibtex.filter = function(data, filter) {
 	if (!(filter instanceof Function)) {
 	    var ft = filter;
@@ -21,7 +46,6 @@ module.exports = function(env, callback) {
 	}
 
 	var fd = data.filter(filter);
-	console.log(fd.length);
 	return fd;
     };
 
